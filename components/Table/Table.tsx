f0d43680bd20c400fc359916537f0f507d849b52
J Caso <jcaso@protonmail.com>
2023-02-13 21:30:44 +0000

    add drawer

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -25,47 +25,63 @@ export const Table: React.FC = () => {
     >
     <h1>Sidechains list</h1>
     {/* @ts-ignore*/}
-    <div role="region" aria-labelledby="caption" tabIndex="0" style={{maxHeight: '600px'}}>
-      <table className="bg-transparent rounded-xl" >
-        <thead className="bordered">
-        <tr >
-          <th className="bg-gray-200 dark:bg-gray-900">Chain</th>
-          <th className="bg-gray-200 dark:bg-gray-900">VM</th>
-          <th className="bg-gray-200 dark:bg-gray-900">Type</th>
-          <th className="bg-gray-200 dark:bg-gray-900">Consensus</th>
-          <th className="bg-gray-200 dark:bg-gray-900">Url</th>
-        </tr>
-        </thead>
-        <tbody>
-        {
-          chains.map((item, index) => {
-            return <> <tr key={index} className="hover:opacity-70">
-              <th className="bg-gray-100 dark:bg-gray-800" onClick={() => onClickChain(item.chain)}>{item.chain}</th>
-              <td>${item.vm}{`${item.vm} ${item.vmVersion}`}</td>
-              <td>{item.type}</td>
-              <td>{`${item.consensus} ${item.consensusVersion}`}</td>
-              <td><a href={item.url} target="_blank" rel="noreferrer">{item.url}</a></td>
+    <div className="drawer drawer-end">
+      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <div role="region" aria-labelledby="caption" tabIndex="0" style={{maxHeight: '600px'}}>
+          <table className="bg-transparent rounded-xl" >
+            <thead className="bordered">
+            <tr >
+              <th className="bg-gray-200 dark:bg-gray-900">Chain</th>
+              <th className="bg-gray-200 dark:bg-gray-900">VM</th>
+              <th className="bg-gray-200 dark:bg-gray-900">Type</th>
+              <th className="bg-gray-200 dark:bg-gray-900">Consensus</th>
+              <th className="bg-gray-200 dark:bg-gray-900">Url</th>
             </tr>
-              {
-                item.open ? <tr className="w-auto">
-                  <th className="bg-gray-100 dark:bg-gray-800"/>
-                  {/* @ts-ignore*/}
-                  <td colSpan="4" className="animate-fade container" style={{maxWidth: '500px'}}>
-                    <div className="p-12 md:w-1/2 flex flex-col items-start">
-                      <span className="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">{item.chain}</span>
-                      <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">{item.goal}</h2>
-                      <p className="leading-relaxed mb-8 ">{item.USPs}</p>
-                      <p className="leading-relaxed mb-8" >{item.description}</p>
-                    </div>
+            </thead>
+            <tbody>
+            {
+              chains.map((item, index) => {
+                return <> <tr key={index} className="hover:opacity-70">
+                  <th className="bg-gray-100 dark:bg-gray-800" onClick={() => onClickChain(item.chain)}>
+                    <label htmlFor="my-drawer-4" className="drawer-button">
+                      {item.chain}
+                    </label>
+                  </th>
+                  <td>${item.vm}{`${item.vm} ${item.vmVersion}`}</td>
+                  <td>{item.type}</td>
+                  <td>{`${item.consensus} ${item.consensusVersion}`}</td>
+                  <td><a href={item.url} target="_blank" rel="noreferrer">{item.url}</a></td>
+                </tr>
+                  {
+                    item.open ? <tr className="w-auto">
+                      <th colSpan="3" className="animate-fade" style={{width: '160px'}}>
+                        <p style={{wordWrap: 'break-word', width: '60px'}} className="flex">
+                          fdsvvd{'\n'}svddsfsdsvdsvdfdsvvdsvddsfsdsvdsvdfdsvvdsv
+                          <br/>ddsfsdsvdsvdfdsvvdsvddsfsdsvdsvdfdsvvdsvddsfsdsvdsvd
+                          <br/>fdsvvdsvddsfsdsvdsvdfdsvvdsvddsfsdsvdsvdfdsvvdsvddsfsdsvdsvd</p>
+                        <br/>
+                        <p className="row-details">
+                          ddsfsdsvdsvdfdsvvdsvddsfsdsvdsvdfdsvvdsvddsfsdsvdsvddds<br/>
+                        </p>
+                      </th>
+                    </tr> : null
+                  }
+                </>
+              })
+            }
+            </tbody>
+          </table>
 
-                  </td>
-                </tr> : null
-              }
-            </>
-          })
-        }
-        </tbody>
-      </table>
+        </div>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
+        <ul className="menu p-4 bg-transparent/80 text-base-content">
+          <li><a>ddsfsdsvdsvdfdsvvdsvddsfsdsvdsvdfdsvvdsvddsfsdsvdsvddds Item 1</a></li>
+          <li><a>Sidebar Item 2</a></li>
+        </ul>
+      </div>
     </div>
     </div>
 
